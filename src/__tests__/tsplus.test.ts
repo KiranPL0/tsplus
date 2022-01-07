89c953496ce5d86136fb66bbdd302268df035803
KiranPL0 <kiranlildhar@icloud.com>
2022-01-07 02:42:13 +0000

    t

--- src/__tests__/tsplus.test.ts
+++ src/__tests__/tsplus.test.ts
@@ -1,5 +1,10 @@
-import { tsplus } from '../index';
+import { print } from '../index';
+import { random } from '../index';
+import { get } from '../index'
 
-test('Kiran', () => {
-    expect(tsplus('Kiran')).toBe('Hello Kiran');
+test('Hi!', () => {
+    expect(print('Hi!')).toBe('Hi!');
+    console.log(random(10))
+    // expect(get('https://kiran.at/w')).toBe('Cannot GET /w')
+    console.log(get('https://kiran.at/w'))
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
 export const tsplus = (name: string) => `Hello ${name}`;
+
+const https = require('https')
+
+
+export function print(arg: any){
+    console.log(arg)
+    return arg
+}
+
+export function random(arg: number){
+    return Math.floor(Math.random() * arg)
+}
+
+export function get(link: string){
+    https.get(link, (res: any) => {
+        let data: any = []
+
+        res.on('data', (chunk: any) => {
+            data.push(chunk)
+        })
+
+        res.on('end', () => {
+            return data
+            
+        })
+        
+    }).on('error', (err: any) => {
+        return "Error: " + err.message
+        console.log("Error: " + err.message)
+    })
+}
